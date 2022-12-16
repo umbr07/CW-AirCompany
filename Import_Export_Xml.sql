7ea3b173899af9fc4a248faec4558a80c0a6ccee
Igor Dikun <dikun9489@gmail.com>
2022-12-16 04:05:33 +0300

    Fully working import/export of xml files

--- Import_Export_Xml.sql
+++ Import_Export_Xml.sql
@@ -1,52 +1,53 @@
 use AirCompany;
 
 --Export products proc
-go 
-create procedure ExProdToXml
+go
+create or alter procedure ExportXML
 as
 begin
-	select Id, Login, Password, FirstName, LastName, Email,Roles from Users
-		for xml path('User'), root('Users');
+	select  Id, Login, Password, FirstName, LastName, Email, Roles from Users
+		for xml path('user'), root('users');
 
-	EXEC master.dbo.sp_configure 'show advanced options', 1
-		reconfigure with override
-	EXEC master .dbo.sp_configure 'xp_cmdshell', 1
+	exec master.dbo.sp_configure 'show advanced options', 1
+		reconfigure with override;
+	exec master.dbo.sp_configure 'xp_cmdshell', 1
 		reconfigure with override;
 
 	declare @fileName nvarchar(100)
 	declare @sqlStr varchar(1000)
 	declare @sqlCmd varchar(1000)
-	
-	set @fileName = 'D:\Учёба\3 курс\1 семестр\CursWork\CourseWork\export.xml';
-	set @sqlStr = 'USE AirCompany; select Id, Login, Password, FirstName, LastName, Email,Roles from Users FOR XML PATH(''User''), ROOT(''Users'') '
-	set @sqlCmd = 'bcp.exe "' + @sqlStr + '" queryout ' + @fileName + ' -w -T'
-	EXEC xp_cmdshell @sqlCmd;
+
+	set @fileName = 'D:\export.xml';
+	set @sqlStr = 'use AirCompany; select  Id, Login, Password, FirstName, LastName, Email, Roles from Users for xml path(''user''), root(''users'') ';
+	set @sqlCmd = 'bcp "' + @sqlStr + '" queryout ' + @fileName + ' -w -T -S WIN-I00JS7JRQF8\RDCB';
+	exec xp_cmdshell @sqlCmd;
 end;
 
-drop procedure ExProdToXml;
+drop procedure ExportXML;
 
-exec ExProdToXml;
+exec ExportXML;
 
---Import product proc 
+--Import user procedure 
 go
-Create Procedure ImProdfromXml
-AS
-Begin
-	INSERT INTO Users (Id, Login, Password, FirstName, LastName, Email, Roles)
-	SELECT
-	   MY_XML.User.query('Id').value('.', 'INT'),
-	   MY_XML.User.query('Login').value('.', 'VARCHAR(50)'),
-	   MY_XML.User.query('Password').value('.', 'VARCHAR(50)'),
-	   MY_XML.User.query('FirstName').value('.', 'NVARCHAR(50)'),
-	   MY_XML.User.query('LastName').value('.', 'NVARCHAR(50)'),
-	   MY_XML.User.query('Email').value('.', 'VARCHAR(50)')
-	   MY_XML.User.query('Roles').value('.', 'INT')
+create or alter procedure ImportFromXML
+as begin
+DECLARE @xml XML;
 
-	FROM (SELECT CAST(MY_XML AS xml)
-		  FROM OPENROWSET(BULK 'E:\3rd-course\1st-sem\DB\CW\Backups\Import.xml', SINGLE_BLOB) AS T(MY_XML)) AS T(MY_XML)
-		  CROSS APPLY MY_XML.nodes('PRODUCTS/PRODUCT') AS MY_XML (PRODUCT);
-End;
+SELECT @xml = CONVERT(xml, BulkColumn, 2) FROM OPENROWSET(BULK 'D:\Учёба\3 курс\1 семестр\CursWork\CourseWork\import.xml', SINGLE_BLOB) AS x
 
-drop procedure ImProdfromXml;
+INSERT INTO  Users(Login, Password, FirstName, LastName, Email, Roles)
+SELECT 
+	t.x.query('Login').value('.', 'varchar(50)') ,
+	t.x.query('Password').value('.', 'varchar(50)'),
+	t.x.query('FirstName').value('.', 'nvarchar(50)'),
+	t.x.query('LastName').value('.', 'nvarchar(50)'),
+	t.x.query('Email').value('.', 'varchar(50)'),
+	t.x.query('Roles').value('.', 'int')
+FROM @xml.nodes('//Users/User') t(x)
+end
 
-Exec ImProdfromXml;
+select * from Users;
+
+drop procedure ImportFromXML;
+
+exec ImportFromXML;
